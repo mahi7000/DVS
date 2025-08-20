2c094601b0fd7e1c70b9d18666c758898e414d73
mahi7000 <mahlet7000@gmail.com>
2025-08-20 16:02:46 +0300

    Add stored xss detection

---
+++ cli/dvs/utils/escapeForDisplay.py
@@ -0,0 +1,6 @@
+def escape_for_display(text: str) -> str:
+    return (text.replace('&', '&amp;')
+                .replace('<', '&lt;')
+                .replace('>', '&gt;')
+                .replace('"', '&quot;')
+                .replace("'", '&#39;'))
